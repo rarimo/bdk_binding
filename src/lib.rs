ab6d9f3367199d729a5a65e869bc9583bb43f868
ivanlele <ivanlele402@gmail.com>
2025-05-27 13:29:57 +0300

    Add bdk_get_address function to retrieve Bitcoin address from private key

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use bdk::bitcoin::{Network, PrivateKey, key::Secp256k1};
+use bdk::bitcoin::{Address, Network, PrivateKey, key::Secp256k1};
 
 #[unsafe(no_mangle)]
 pub extern "C" fn bdk_get_public_key(data: *const u8, len: usize, out_len: *mut usize) -> *mut u8 {
@@ -18,12 +18,42 @@ pub extern "C" fn bdk_get_public_key(data: *const u8, len: usize, out_len: *mut 
     output_ptr
 }
 
+#[unsafe(no_mangle)]
+pub extern "C" fn bdk_get_address(data: *const u8, len: usize, out_len: *mut usize) -> *mut u8 {
+    let private_key_data = unsafe {
+        assert!(!data.is_null(), "Input data pointer is null");
+        std::slice::from_raw_parts(data, len)
+    };
+
+    let address = get_address(private_key_data.to_vec());
+
+    let output_ptr = address.as_ptr() as *mut u8;
+
+    unsafe {
+        *out_len = address.len();
+    }
+
+    output_ptr
+}
+
 fn get_public_key(private_key_data: Vec<u8>) -> Vec<u8> {
-    let private_key = PrivateKey::from_slice(&private_key_data, Network::Bitcoin)
+    let private_key = PrivateKey::from_slice(&private_key_data, Network::Regtest)
         .expect("Failed to create PrivateKey from slice");
 
     let secp = Secp256k1::new();
     let public_key = private_key.public_key(&secp);
 
     return public_key.to_bytes();
+}
+
+fn get_address(private_key_data: Vec<u8>) -> Vec<u8> {
+    let private_key = PrivateKey::from_slice(&private_key_data, Network::Regtest)
+        .expect("Failed to create PrivateKey from slice");
+
+    let secp = Secp256k1::new();
+    let public_key = private_key.public_key(&secp);
+
+    let address = Address::p2pkh(&public_key, Network::Bitcoin);
+
+    return address.to_string().as_bytes().to_vec();
 }
